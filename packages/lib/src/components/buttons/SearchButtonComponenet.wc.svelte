8feab7bde4bfdf97db513f4b6ff1b462e711a281
Mats Johansen <mats.johansen@dkfz-heidelberg.de>
2023-09-04 15:44:13 +0200

    feat(search button): can be disabled

--- packages/lib/src/components/buttons/SearchButtonComponenet.wc.svelte
+++ packages/lib/src/components/buttons/SearchButtonComponenet.wc.svelte
@@ -1,33 +1,33 @@
-<svelte:options customElement="lens-search-button" />
+<svelte:options customElement="lens-search-button"/>
 
 <script lang="ts">
     import { responseStore } from "../../stores/response";
     export let title: string = "Search";
-    export let url: string = ''
+    export let url: string = "";
+    export let disabled: boolean = false;
 
     const getResultsFromBiobanks = async () => {
         /**
          * TODO: replace with actual fetch call
-         * 
+         *
          * writes the response to the responseStore
          * response.text() with the try catch statenent prevents server responses like 500 to parse html to json
-        */
-        const response = await fetch(url).then(response => {
-        if (!response.ok) {
-            throw new Error('Network error');
-        }
-        return response.text();
-        })
+         */
+        const response = await fetch(url).then((response) => {
+            if (!response.ok) {
+                throw new Error("Network error");
+            }
+            return response.text();
+        });
 
         try {
-           $responseStore = JSON.parse(response);
+            $responseStore = JSON.parse(response);
         } catch (error) {
-           console.error(error);
+            console.error(error);
         }
-
     };
 </script>
 
-<button part="lens-search-button" on:click={getResultsFromBiobanks}>
+<button part="lens-search-button" on:click={getResultsFromBiobanks} {disabled}>
     {title}
 </button>
