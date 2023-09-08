d602f7939cf0ea308a354fd391dae6dfd3bbccd1
Mats Johansen <mats.johansen@dkfz-heidelberg.de>
2023-09-08 18:20:09 +0200

    feat(charts): add chart component

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -3,3 +3,4 @@ export { default as QueryDisplayComponent } from './src/components/query-display
 export { default as SearchBarComponent } from './src/components/search-bar/SearchBarComponent.wc.svelte';
 export {default as StateDisplayComponent} from './src/components/testing-components/StateDisplayComponent.wc.svelte';
 export {default as SearchButtonComponent} from './src/components/buttons/SearchButtonComponenet.wc.svelte'
+export {default as ChartComponent} from './src/components/results/ChartComponent.wc.svelte'
--- packages/lib/src/components/results/ChartComponent.wc.svelte
+++ packages/lib/src/components/results/ChartComponent.wc.svelte
@@ -1,1 +1,38 @@
-<svelte:options customElement={{ tag: "lens-chart", props: { data: { type: "Object" } } }} />
+<svelte:options customElement={{
+    tag: "lens-chart",
+    props: {
+        chartData: { type: "Object" },
+        },
+}} />
+
+<script lang="ts">
+    import Chart from "chart.js/auto";
+    import { onMount } from "svelte";
+
+    export let title: string = "";
+    export let hintText: string = "";
+
+    /**
+     * TODO: split options and data into two props
+     * data will come from the store
+     * options will be set from outside the component
+     */
+    export let chartData: any = {};
+
+    let canvas!: HTMLCanvasElement;
+
+    /**
+     * initialize the chart on mount
+    */
+    onMount(() => {
+        new Chart(canvas, chartData);
+    });
+</script>
+
+<div part="chart-wrapper">
+    <h4 part="chart-title">{title}</h4>
+    <div part="canvas-container">
+        <canvas part="chart-canvas" bind:this={canvas} id="chart" />
+    </div>
+    <div part="chart-hint">{hintText}</div>
+</div>
