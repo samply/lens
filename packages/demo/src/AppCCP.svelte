c2e08322f3cc9dced0362e125dfdb08e2f0753b4
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2024-01-11 10:16:06 +0100

    Added sub cre

--- packages/demo/src/AppCCP.svelte
+++ packages/demo/src/AppCCP.svelte
@@ -143,7 +143,7 @@
   const genderHeaders: Map<string, string> = new Map<string, string>()
     .set("male", "männlich")
     .set("female", "weiblich")
-    .set("other", "divers, intersexuell")
+    .set("other", "Divers, Intersexuell")
     .set("unknown", "unbekannt");
 
   const vitalStateHeaders: Map<string, string> = new Map<string, string>()
