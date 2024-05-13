96921ae007a81e168bb3d8c1456f3e244f79a85c
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2024-05-13 15:23:39 +0200

    feat: added bbmri cql

--- packages/lib/src/cql-translator-service/ast-to-cql-translator.ts
+++ packages/lib/src/cql-translator-service/ast-to-cql-translator.ts
@@ -119,8 +119,11 @@ const getSingleton = (criterion: AstBottomLayerValue): string => {
         if (myCQL) {
             switch (myCriterion.type) {
                 case "gender":
+                case "BBMRI_gender":
                 case "histology":
                 case "conditionValue":
+                case "BBMRI_conditionValue":
+                case "BBMRI_conditionSampleDiagnosis":
                 case "conditionBodySite":
                 case "conditionLocalization":
                 case "observation":
@@ -131,6 +134,8 @@ const getSingleton = (criterion: AstBottomLayerValue): string => {
                 case "procedureResidualstatus":
                 case "medicationStatement":
                 case "specimen":
+                case "BBMRI_specimen":
+                case "BBMRI_hasSpecimen":
                 case "hasSpecimen":
                 case "Organization":
                 case "observationMolecularMarkerName":
