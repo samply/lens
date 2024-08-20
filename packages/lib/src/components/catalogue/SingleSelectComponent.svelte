e8ce240118473d9ffd50746e23c0f012372465c1
Zachariah Frank <zachariahjfrank@gmail.com>
2024-08-20 16:06:57 +0200

    fix(SingleSelectComponent): remove redundant 'Add All'

--- packages/lib/src/components/catalogue/SingleSelectComponent.svelte
+++ packages/lib/src/components/catalogue/SingleSelectComponent.svelte
@@ -1,58 +1,12 @@
 <script lang="ts">
-    import { iconStore } from "../../stores/icons";
-    import { addItemToQuery } from "../../stores/query";
-    import type { QueryItem } from "../../types/queryData";
     import type { Category } from "../../types/treeData";
     import SingleSelectItemComponent from "./SingleSelectItemComponent.svelte";
-    import { v4 as uuidv4 } from "uuid";
 
     export let element: Category;
-    export let subgrouping: boolean = false;
-
-    const addAll = (): void => {
-        if (!("criteria" in element)) return;
-        element.criteria.forEach((criterion) => {
-            const queryItem: QueryItem = {
-                id: uuidv4(),
-                key: element.key,
-                name: element.name,
-                type: "type" in element && element.type,
-                values: [
-                    {
-                        name: criterion.name,
-                        value:
-                            "aggregatedValue" in criterion
-                                ? criterion.aggregatedValue
-                                : criterion.key,
-                        queryBindId: uuidv4(),
-                    },
-                ],
-            };
-            addItemToQuery(queryItem, 0);
-        });
-    };
 </script>
 
 <div part="criterion-wrapper single-select-wrapper">
     {#if "criteria" in element}
-        {#if subgrouping && element.criteria.length > 1}
-            <div part="criterion-section criterion-section-values">
-                <span part="criterion-single-select-name">
-                    alle hinzufügen
-                </span>
-            </div>
-            <button part="query-add-button" on:click={addAll}>
-                {#if $iconStore.get("addIconUrl")}
-                    <img
-                        part="query-add-button-icon"
-                        src={$iconStore.get("addIconUrl")}
-                        alt="add icon"
-                    />
-                {:else}
-                    &#8594;
-                {/if}
-            </button>
-        {/if}
         {#each element.criteria as criterion}
             <SingleSelectItemComponent {element} {criterion} />
         {/each}
