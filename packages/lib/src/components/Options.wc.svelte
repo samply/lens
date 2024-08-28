d2c0bd37498e7e35cdeda6daf0d064c24fcf88d5
Mats Johansen <mats.johansen@dkfz-heidelberg.de>
2024-08-28 12:50:34 +0200

    feat(options): move catalog response key map

--- packages/lib/src/components/Options.wc.svelte
+++ packages/lib/src/components/Options.wc.svelte
@@ -23,7 +23,10 @@
     import catalogueSchema from "../types/catalogue.schema.json";
     import { parser } from "@exodus/schemasafe";
     import type { LensOptions } from "../types/options";
-    import { uiSiteMappingsStore } from "../stores/mappings";
+    import {
+        catalogueKeyToResponseKeyMap,
+        uiSiteMappingsStore,
+    } from "../stores/mappings";
 
     export let options: LensOptions = {};
     export let catalogueData: Criteria[] = [];
@@ -120,6 +123,17 @@
             mappings.set(site[0], site[1]);
         });
 
+        return mappings;
+    });
+
+    $: catalogueKeyToResponseKeyMap.update((mappings) => {
+        if (!options?.catalogueKeyToResponseKeyMap) return mappings;
+        console.log(options.catalogueKeyToResponseKeyMap);
+
+        options.catalogueKeyToResponseKeyMap.forEach((mapping) => {
+            console.log(mapping);
+            mappings.set(mapping[0], mapping[1]);
+        });
         return mappings;
     });
 
