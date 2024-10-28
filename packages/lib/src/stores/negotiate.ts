7357aef451f8416c9b3705507e1ea5d20d2159b5
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2024-10-28 07:52:35 +0100

    fix: info and search button with empty second empty query input

--- packages/lib/src/stores/negotiate.ts
+++ packages/lib/src/stores/negotiate.ts
@@ -53,28 +53,30 @@ export const buildHumanReadableRecursively = (
     }
 
     queryLayer.children.forEach((child: AstElement, index: number): void => {
-        if ("type" in child && "value" in child && "key" in child) {
-            if (typeof child.value === "string") {
-                humanReadableQuery += `(${child.key} ${child.type} ${child.value})`;
+        if (child !== null) {
+            if ("type" in child && "value" in child && "key" in child) {
+                if (typeof child.value === "string") {
+                    humanReadableQuery += `(${child.key} ${child.type} ${child.value})`;
+                }
+                if (
+                    typeof child.value === "object" &&
+                    "min" in child.value &&
+                    "max" in child.value
+                ) {
+                    humanReadableQuery += `(${child.key} ${child.type} ${child.value.min} and ${child.value.max})`;
+                }
             }
-            if (
-                typeof child.value === "object" &&
-                "min" in child.value &&
-                "max" in child.value
-            ) {
-                humanReadableQuery += `(${child.key} ${child.type} ${child.value.min} and ${child.value.max})`;
+
+            humanReadableQuery = buildHumanReadableRecursively(
+                child,
+                humanReadableQuery,
+            );
+
+            if (index === queryLayer.children.length - 1) {
             }
-        }
-
-        humanReadableQuery = buildHumanReadableRecursively(
-            child,
-            humanReadableQuery,
-        );
-
-        if (index === queryLayer.children.length - 1) {
-        }
-        if (index < queryLayer.children.length - 1) {
-            humanReadableQuery += ` ${queryLayer.operand} `;
+            if (index < queryLayer.children.length - 1) {
+                humanReadableQuery += ` ${queryLayer.operand} `;
+            }
         }
     });
 
