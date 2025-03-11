aa96f5ddebff8aeb10e2f457e762846073e49875
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-03-11 15:55:42 +0100

    chore: fix result table
    Also fix husky deprecation error

--- packages/lib/src/components/results/TableItemComponent.svelte
+++ packages/lib/src/components/results/TableItemComponent.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import { run } from "svelte/legacy";
-
     import { datarequestsStore } from "../../stores/datarequests";
 
     interface Props {
@@ -9,10 +7,9 @@
 
     let { tableRow }: Props = $props();
 
-    let checked: boolean = $state(false);
-    run(() => {
-        checked = $datarequestsStore.includes(tableRow[0] as string);
-    });
+    let checked: boolean = $derived(
+        $datarequestsStore.includes(tableRow[0] as string),
+    );
 
     /**
      * adds and removes tableRows from the negotiateStore whenever the checkbox is checked or unchecked
@@ -22,8 +19,7 @@
             datarequestsStore.update((store: string[]) => {
                 return [...store, tableRow[0] as string];
             });
-        }
-        if (checked) {
+        } else {
             datarequestsStore.update((store: string[]) => {
                 return store.filter((site: string) => site !== tableRow[0]);
             });
@@ -36,7 +32,7 @@
         ><input
             part="table-body-checkbox"
             type="checkbox"
-            bind:checked
+            {checked}
             onchange={updateStoreOnCheck}
         /></td
     >
