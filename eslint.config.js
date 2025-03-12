f5b1481c5dc62cb171a48da87df93aa2856298e3
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-03-12 17:37:15 +0100

    chore: add ESLint again
    The old config format is deprecated. Because the migration script did not work I recreated the config in the new format. Some lints that were manually enabled earlier may be missing. For now we have the recommended defaults:
    * JS and TS lints that are included in ESLint
    * Recommend lints from eslint-plugin-svelte

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import svelte from "eslint-plugin-svelte";
+import svelteConfig from "./svelte.config.js";
+
+export default defineConfig([
+    globalIgnores(["dist", "packages/demo/dist"]),
+    // Recommended JavaScript and TypeScript lints
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    // Configure ESLint for use with Svelte, see https://github.com/sveltejs/eslint-plugin-svelte#typescript-project
+    ...svelte.configs.recommended,
+    { languageOptions: { globals: globals.browser } },
+    {
+        files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                extraFileExtensions: [".svelte"],
+                parser: tseslint.parser,
+                svelteConfig,
+            },
+        },
+    },
+    // Turn off lints that conflict with Prettier, see https://github.com/prettier/eslint-config-prettier#installation
+    eslintConfigPrettier,
+]);
