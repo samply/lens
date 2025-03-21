e6aa6505f7aca2c3550e872752bd376ef0d861e5
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-03-21 12:56:06 +0100

    chore: fix generate-json-schema.bash script

--- scripts/generate-json-schema.bash
+++ scripts/generate-json-schema.bash
@@ -1,6 +1,6 @@
 if [ "$1" = "--check" ]; then
     set -e # Fail immediately if one of the comparisons fails
-    ts-json-schema-generator --path packages/lib/src/types/catalogue.ts --type Catalogue | cmp - schema/catalogue.schema.json
+    npx ts-json-schema-generator --path src/types/catalogue.ts --type Catalogue | cmp - schema/catalogue.schema.json
 else
-    ts-json-schema-generator --path packages/lib/src/types/catalogue.ts --type Catalogue > schema/catalogue.schema.json
+    npx ts-json-schema-generator --path src/types/catalogue.ts --type Catalogue > schema/catalogue.schema.json
 fi
