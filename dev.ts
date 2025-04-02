b6bef5903c56648405af5b225bf46a1f8be57c8c
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-04-02 11:48:51 +0200

    chore: log AST to console when searching in dev.ts

--- dev.ts
+++ dev.ts
@@ -184,6 +184,7 @@ setCatalogue([
 
 window.addEventListener("emit-lens-query", (event) => {
     const detail = (event as QueryEvent).detail;
+    console.log("AST:", JSON.stringify(detail.ast));
     detail.updateResponse(new Map([["riverside", makeSite(5, 4, 0)]]));
     detail.updateResponse(new Map([["summit", makeSite(12, 18, 3)]]));
 });
