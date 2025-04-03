849f751a0dca07ce50c1fe06b26a1e5951ddfb9f
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-04-03 14:42:49 +0200

    chore: enable runes mode to force us to use Svelte 5 syntax

--- svelte.config.js
+++ svelte.config.js
@@ -2,5 +2,8 @@ export default {
     compilerOptions: {
         // See https://svelte.dev/docs/svelte/custom-elements
         customElement: true,
+        // Force all components into Svelte 5 mode and make Svelte 4 syntax unavailable.
+        // This will probably become the default at some point and then we can remove this.
+        runes: true,
     },
 };
