afc43c00222d2ac64793ef4c1335f8663e356d62
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-06-06 13:01:53 +0200

    chore: fix lens dev layout

--- dev.svelte
+++ dev.svelte
@@ -132,11 +132,6 @@
                     name: "B-",
                     description: "",
                 },
-                {
-                    key: "A++",
-                    name: "This is a really long text and tests if the css works. It should do a break about here, I hope",
-                    description: "",
-                },
             ],
         },
         {
@@ -553,21 +548,25 @@
     }
 </script>
 
-<header>
-    <h2>Lens Dev</h2>
+<header style="padding: 10px;">
+    <h2 style="margin: 0;">Lens Dev</h2>
 </header>
-<div class="searchbar">
-    <lens-search-bar-multiple></lens-search-bar-multiple>
+
+<div style="display: flex; padding: 10px; gap: 10px;">
+    <div style="flex: 1">
+        <lens-search-bar-multiple></lens-search-bar-multiple>
+    </div>
     <lens-info-button noQueryMessage="Empty Query" showQuery={true}
     ></lens-info-button>
-    <lens-query-spinner></lens-query-spinner>
     <lens-search-button></lens-search-button>
+    <lens-query-spinner></lens-query-spinner>
 </div>
-<div class="container">
-    <div class="box">
+
+<div style="display: flex; padding: 10px; gap: 10px;">
+    <div style="flex: 1">
         <lens-catalogue toggle={{ open: true }}></lens-catalogue>
     </div>
-    <div class="box2">
+    <div style="flex: 1">
         <lens-result-summary></lens-result-summary>
         <lens-negotiate-button title="Request Data"></lens-negotiate-button>
         <lens-search-modified-display></lens-search-modified-display>
@@ -596,49 +595,25 @@
         ></lens-chart>
     </div>
 </div>
-<button
-    id="error-toast-test-button"
-    onclick={() => showErrorToast(translate("lens-dev-test-error"))}
-    >Error toast test</button
->
-<button
-    id="switch-language-to-german-button"
-    onclick={() => setLangAndReload("de")}>🇩🇪</button
-><button
-    id="switch-language-to-english-button"
-    onclick={() => setLangAndReload("en")}>🇬🇧</button
->
-<error-toasts></error-toasts>
 
-<footer>
-    Made with ♥ and
-    <a href="https://github.com/samply/lens">samply/lens</a>.
+<footer style="display: flex; padding: 10px; gap: 10px;">
+    <button
+        id="error-toast-test-button"
+        onclick={() => showErrorToast(translate("lens-dev-test-error"))}
+        >Error toast test</button
+    >
+    <button
+        id="switch-language-to-german-button"
+        onclick={() => setLangAndReload("de")}>🇩🇪</button
+    ><button
+        id="switch-language-to-english-button"
+        onclick={() => setLangAndReload("en")}>🇬🇧</button
+    >
+    <span
+        >Made with ♥ and <a href="https://github.com/samply/lens"
+            >samply/lens</a
+        >.</span
+    >
 </footer>
 
-<style>
-    :root {
-        padding-left: 100px;
-        padding-right: 100px;
-    }
-    header {
-        background-color: ghostwhite;
-    }
-    footer {
-        background-color: ghostwhite;
-    }
-    .container {
-        display: flex;
-    }
-    .searchbar {
-        display: grid;
-        grid-template-columns: 25fr 1fr 1fr 1fr;
-        align-items: center;
-    }
-    .box {
-        padding: 10px;
-        width: 600px;
-    }
-    .box2 {
-        flex-grow: 1;
-    }
-</style>
+<error-toasts></error-toasts>
