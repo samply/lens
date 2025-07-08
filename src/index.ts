251dc172fe2c3e8064367e52c53c8764be73171f
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-07-08 11:24:43 +0200

    docs: add removal of LensDataPasser to migration guide

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,7 @@ export type {
 } from "./types/backend";
 export type { SiteData, Site } from "./types/response";
 export type { QueryItem, QueryValue } from "./types/queryData";
+export type { AddStratifierParams } from "./stores/query";
 
 // Include custom elements (aka web components) in the bundle
 import "./components/catalogue/Catalogue.wc.svelte";
