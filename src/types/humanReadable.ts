3a2f23174c086a79564acff6f2880c2969bb2c1f
Mats Johansen <mats.johansen@dkfz-heidelberg.de>
2025-09-03 11:07:49 +0200

    fix(human readable): fix reactivity to query store

--- src/types/humanReadable.ts
+++ src/types/humanReadable.ts
@@ -1,6 +1,9 @@
+import type { QueryItem } from "./queryData";
+
 export interface GetHumanReadableQuery {
     useFullAggregatedValues?: boolean;
     getObject?: boolean;
+    queryStore: QueryItem[][];
 }
 
 export type HumanReadableQueryObject = {
