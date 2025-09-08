01b6ac0bb4cbe075f487077cdc0ad6a3beccd11a
Mats Johansen <mats.johansen@dkfz-heidelberg.de>
2025-09-08 11:42:26 +0200

    fix(human readable): add legacy function for backwards compatibility

--- src/stores/datarequests.ts
+++ src/stores/datarequests.ts
@@ -4,6 +4,8 @@ import type { AggregatedValue } from "../types/catalogue";
 import { queryStore } from "./query";
 import { translate } from "../helpers/translations";
 import { catalogue, getCategoryFromKey } from "./catalogue";
+import { buildAstFromQuery } from "../helpers/ast-transformer";
+import type { AstElement, AstTopLayer } from "../types/ast";
 
 export const datarequestsStore = writable<string[]>([]);
 
@@ -12,11 +14,97 @@ export function getSelectedSites(): string[] {
     return get(datarequestsStore);
 }
 
+const catalogueForNameExtraction = get(catalogue);
+
+/**
+ * Recursively builds a human readable query string from the AST
+ * Legacy function, currently used for bbmri negotiator
+ * prints out logical structure of the query, not the formatted version
+ * @param queryLayer the current layer of the query
+ * @param humanReadableQuery string to append to
+ * @returns a human readable query string
+ */
+export const buildHumanReadableRecursively = (
+    queryLayer: AstElement,
+    humanReadableQuery: string,
+): string => {
+    if (
+        queryLayer === null ||
+        !("children" in queryLayer) ||
+        ("children" in queryLayer &&
+            (queryLayer.children === null ||
+                queryLayer.children.length === 0 ||
+                queryLayer.children[0] === null))
+    ) {
+        return humanReadableQuery;
+    }
+
+    if (queryLayer.children.length > 1) {
+        humanReadableQuery += "(";
+    }
+
+    queryLayer.children.forEach((child: AstElement, index: number): void => {
+        if (child !== null) {
+            if ("type" in child && "value" in child && "key" in child) {
+                if (typeof child.value === "string") {
+                    humanReadableQuery += `(${child.key} ${child.type} ${child.value})`;
+                }
+                if (
+                    typeof child.value === "object" &&
+                    !Array.isArray(child.value) &&
+                    ("min" in child.value || "max" in child.value)
+                ) {
+                    if (
+                        child.value.min !== undefined &&
+                        child.value.max !== undefined
+                    ) {
+                        humanReadableQuery += `(${child.key} from ${child.value.min} to ${child.value.max})`;
+                    } else if (child.value.min !== undefined) {
+                        humanReadableQuery += `(${child.key} greater than or equal to ${child.value.min})`;
+                    } else if (child.value.max !== undefined) {
+                        humanReadableQuery += `(${child.key} less than or equal to ${child.value.max})`;
+                    }
+                }
+            }
+
+            humanReadableQuery = buildHumanReadableRecursively(
+                child,
+                humanReadableQuery,
+            );
+
+            if (index < queryLayer.children.length - 1) {
+                humanReadableQuery += ` ${queryLayer.operand} `;
+            }
+        }
+    });
+
+    if (queryLayer.children.length > 1) {
+        humanReadableQuery += ")";
+    }
+
+    return humanReadableQuery;
+};
+
+/**
+ * @returns a human readable query string built from the current query
+ */
+export const getHumanReadableQuery = (): string => {
+    let humanReadableQuery: string = "";
+
+    queryStore.subscribe((value: QueryItem[][]) => {
+        const query: AstTopLayer = buildAstFromQuery(value);
+        humanReadableQuery = buildHumanReadableRecursively(
+            query,
+            humanReadableQuery,
+        );
+    });
+
+    return humanReadableQuery;
+};
+
 interface GetHumanReadableQueryAsFormattedString {
     printAggregatedValues?: boolean;
 }
-
-const catalogueForNameExtraction = get(catalogue);
 
 /**
  * Formats the query into a human-readable string.
