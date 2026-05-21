f7780b4eed53f4d352cfce85227ba3cdd2b37c8e
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2026-05-21 14:23:41 +0200

    Fix Tailwind CSS in demo

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     build: {
         outDir: "demo",
     },
     base: "./",
-    plugins: [svelte()],
+    plugins: [tailwindcss(), svelte()],
 });
